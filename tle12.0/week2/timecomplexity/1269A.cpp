0a25a99e84041bbc78ff4a2a8d66aafc051812fa
s_rk <sudarshanrk3674@gmail.com>
2025-11-18 21:46:47 +0530

    tc problem solving

---
+++ tle12.0/week2/timecomplexity/1269A.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll= long long;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+     ll n;cin>>n;
+    cout<<9*n<<" "<<8*n<<endl;//Multiplication of a composite no with a no is also a composite no...
+    return 0;
+}
--- tle12.0/week2/timecomplexity/1374A.cpp
+++ tle12.0/week2/timecomplexity/1374A.cpp
@@ -1,11 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
+using ll = long long;
 int main(){
-
-
-
-
-
-
-    return 0;
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        int t;cin>>t;
+        while(t--){
+            ll x,y,n;
+            cin>>x>>y>>n;
+            ll q=(n-y)/x;//k%x==y i.e y is remainder
+            ll k=x*q+y;//Remainder theorem:k=x*q+y //Dividend=Div*Quotient +Rem
+            cout<<k<<endl;
+                   }
+    return 0;//Time complexity is 0(1);Direct formula used 
 }
